efdd99ba234a3b84844e666cf9a9c453c06c2367
kjh98 <kjh48961@naver.com>
2023-08-08 16:14:05 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!--  html이 들어올 자리 -->
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<!-- // 자바스크립트가 들어올 자리 -->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<!-- // 스타일이 들어올 자리 -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
